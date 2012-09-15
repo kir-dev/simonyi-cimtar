4f9be81ebe31f7da6690e47b7ec37b8dabcee125
Balazs Varga <balo.software@gmail.com>
2012-09-15 22:34:33 +0200

    Create basic rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SzkCimtar::Application.config.secret_token = '41ecde19397ffaee9e6f42ca2ef45f53ebe3dc7a2f029927687630599d2f01504f8d565511283f114fdc931bdd5d384ee663a7f4146c04d6d93259b74cf5698e'
