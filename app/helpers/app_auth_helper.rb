6aa3b383b7b5039de230f14c6f6deded69bff494
Balazs Varga <balo.software@gmail.com>
2012-10-13 18:44:19 +0200

    Refactored and testable application controller with tests and fixtures

--- app/helpers/app_auth_helper.rb
+++ app/helpers/app_auth_helper.rb
@@ -7,30 +7,29 @@ module AppAuthHelper
   ENTITLEMENT_SZK = 'urn:geant:niif.hu:sch.bme.hu:entitlement:tag:Simonyi Károly Szakkollégium:16'
 
   def member_attributes
-    ['login', 'email', 'full_name', 'nick']
+    %w(login email full_name nick)
   end
 
   def attribute_mapping
-    {'login' => 'HTTP_UID',
-     'email' => 'HTTP_EMAIL',
-     'full_name' => 'HTTP_COMMON_NAME',
-     'nick' => 'HTTP_NICKNAME',
-     'entitlement' => 'HTTP_EDUPERSONENTITLEMENT'
-    }
+    {login: 'HTTP_UID',
+     email: 'HTTP_EMAIL',
+     full_name: 'HTTP_COMMON_NAME',
+     nick: 'HTTP_NICKNAME',
+     entitlement: 'HTTP_EDUPERSONENTITLEMENT'}
   end
 
   def set_attributes(member)
     member_attributes.each do |attr|
-      member.send(attr + "=", (get_attribute_value attr))
+      member.send(attr + "=", (get_attribute_value attr.to_sym))
     end
   end
 
   def remote_user
-    get_attribute_value 'login'
+    get_attribute_value :login
   end
 
   def is_member_on_vir
-    entitlement = get_attribute_value 'entitlement'
+    entitlement = get_attribute_value :entitlement
     if !entitlement.nil? and entitlement.include? ENTITLEMENT_SZK
       true
     else
@@ -38,9 +37,9 @@ module AppAuthHelper
     end
   end
 
-  def get_attribute_value(attribute_name)
-    if attribute_mapping.has_key?(attribute_name)
-      request.env[attribute_mapping[attribute_name]]
+  def get_attribute_value(attr_sym)
+    if attribute_mapping.has_key?(attr_sym)
+      request.env[attribute_mapping[attr_sym]]
     else
       nil
     end
---
+++ test/functional/app_controller_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class AppControllerTest < ActionController::TestCase
+  include AppAuthHelper
+
+  setup do
+    @users = YAML.load_file(Rails.root.join("test/functional/sso_users.yml"))
+    @controller = HomeController.new
+    get :index
+    clear_test_data
+  end
+
+  test "login with no sso user" do
+    @controller.send(:authenticate_logic)
+    assert_response 302
+    assert_equal Rails.application.config.login_url, @controller.response.header['Location']
+  end
+
+  test "login with a not registered but member on vir user aka newuser" do
+    set_test_data @users[0]
+    assert_equal :goto_reg, @controller.send(:authenticate_logic)
+  end
+
+  test "login with a registered and member on vir user aka user" do
+    set_test_data @users[1]
+    assert_equal :ok, @controller.send(:authenticate_logic)
+  end
+
+  test "login with a not registered and not member on vir user aka hacker" do
+    set_test_data @users[2]
+    assert_equal :access_denied, @controller.send(:authenticate_logic)
+  end
+
+  test "login with a registered and not member on vir user aka oldboy" do
+    set_test_data @users[3]
+    assert_equal :ok, @controller.send(:authenticate_logic)
+  end
+
+  private
+  def set_test_data(sso_user)
+    member_attributes.each do |attr|
+      @controller.request.env[attribute_mapping[attr.to_sym]] = sso_user[attr]
+    end
+    @controller.request.env[attribute_mapping[:entitlement]] = sso_user['entitlement']
+  end
+
+  private
+  def clear_test_data()
+    member_attributes.each do |attr|
+      @controller.request.env[attribute_mapping[attr.to_sym]] = nil
+    end
+    @controller.request.env[attribute_mapping[:entitlement]] = nil
+  end
+end
