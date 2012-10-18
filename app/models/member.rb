889631323a1703eb8a25602edad6806ce50c10d6
Balazs Varga <balo.software@gmail.com>
2012-10-19 00:45:38 +0200

    set the validators on member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,16 +1,21 @@
 class Member < ActiveRecord::Base
-  #attr_accessible :address, :email, :fullName, :hobby, :intro, :phone, :room, :univYear
+  attr_readonly :login
+  attr_accessible :email, :full_name, :nick, :address, :hobby, :intro, :phone, :univ_year, :room
   #
   has_many :memberships, class_name: 'MemberShip', foreign_key: :member_id
   has_many :posts, class_name: 'MemberPost', foreign_key: :member_id
 
   # mandatory fields
-  validates :full_name, :email, :presence => true, :length => {:maximum => 130}
-  validates :email, :uniqueness => {:case_sensitive => false}, :email => true
-  validates :univ_year, :presence => true, :numericality => {:greater_than => 1970, :less_than => 2030}
+  validates :full_name, :email, :login, :presence => true, :length => {:maximum => 130}
+  validates :email, :email => true, :uniqueness => {:case_sensitive => false}
+  validates :login, :uniqueness => {:case_sensitive => false}
+
+  validates :univ_year, :on => :update,
+            :numericality => {:only_integer => true, :greater_than => 1950, :less_than => 2020}
 
   validates :address, :hobby, :length => {:maximum => 250}
   validates :intro, :length => {:maximum => 1000}
   validates :phone, :length => {:maximum => 12}
   validates :room, :length => {:maximum => 10}
+  validates :nick, :length => {:maximum => 100}
 end
