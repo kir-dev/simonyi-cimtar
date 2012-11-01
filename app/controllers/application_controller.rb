24eabc1a745e9512eaeec53d7c5533807df6a046
Balazs Varga <balo.software@gmail.com>
2012-11-01 17:42:08 +0100

    render 404 page when routing error throwed in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   if Rails.env.production?
+    rescue_from ActionController::RoutingError, :with => :render_404
     before_filter :authenticate
   else
     if Rails.env.development?
@@ -59,6 +60,15 @@ class ApplicationController < ActionController::Base
         end
       end
     end
+  end
+
+  private
+  def render_404(exception = nil)
+    if exception
+      logger.info "Rendering 404: #{exception.message}"
+    end
+
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
 
   private
