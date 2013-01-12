76fa505885921a2b2f71383b3519ef3d038cdbc7
Tamas Michelberger <tomi.michel@gmail.com>
2013-01-12 22:49:08 +0100

    view and action for editing MemberPosts

--- app/assets/javascripts/group_admin/member_posts.js
+++ app/assets/javascripts/group_admin/member_posts.js
@@ -1,8 +1,5 @@
-$(document).on('nested:fieldAdded', function(event){
-  // this field was just inserted into your form
-  var field = event.field; 
-  // if "manage" link clicked, check all the checkboxes
-  field.find("a[name=manage_link]").click(function(e) {
-    $(this).siblings("label").children("input").prop("checked", true);
-  });
+$(function() {
+    $("a[name=manage_link]").live("click", function(e) {
+        $(this).siblings("label").children("input").prop("checked", true);
+    });
 });
--- app/controllers/group_admin/member_posts_controller.rb
+++ app/controllers/group_admin/member_posts_controller.rb
@@ -34,6 +34,24 @@ class GroupAdmin::MemberPostsController < ApplicationController
     end
   end
 
+  # GET /group_admin/memberships/1/posts/1/edit
+  def edit
+    @post = MemberPost.find params[:id]
+    authorize! :update, MemberPost, @post.membership.group
+  end
+
+  # PUT /group_admin/memberships/1/posts/1
+  def update
+    @post = MemberPost.find params[:id]
+    authorize! :update, MemberPost, @post.membership.group
+
+    if @post.update_attributes(params[:member_post])
+      redirect_to group_admin_membership_posts_path(@post.membership)
+    else
+      render :edit
+    end
+  end
+
 private 
   def setup_resources_for_posts
     @resources = {}
