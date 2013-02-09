2ce731f883851f0597bc5973386eab683b7cd9bb
Tamas Michelberger <tomi.michel@gmail.com>
2013-02-09 15:07:08 +0100

    post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :membership
   attr_accessible :from, :title, :to
+
+  validates :title, :from, :presence => true
+
+  validates_date :from,
+                 :on_or_after => lambda { Date.new(1990, 1, 1) },
+                 :on_or_before => lambda { Date.current }
+
+  validates_date :to,
+                 :on_or_after => :from,
+                 :on_or_before => lambda { Date.current },
+                 :allow_nil => true
+
 end
